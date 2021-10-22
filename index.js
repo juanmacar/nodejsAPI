52d6ff5db7d07f75cb808cb6f6845850952ccc9f
Juan Macarlupu <juanmacarlupu@gmail.com>
2021-10-21 23:19:49 -0300

    Verbos HTTTP implementados

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let express = require('express');
 const app = express();
 let cors = require('cors');
-const PORT = 3000
 const router = require('./routes/routes');
 require("dotenv").config()
+app.use(express.json());
+
+
+
 app.listen(process.env.PORT, function () {
     console.log(`listening on port http://localhost:${process.env.PORT}`);
 })
 
 app.get('/', function (req, res) {
-    res.send("hola, tenés que entrar a /api");
+    res.send("hola, tenés que entrar a /api y mandar tus peticiones");
 
 })
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,54 @@
 const express = require('express'),
     router = express.Router();
+    
+    let productos = [
+        {
+            nombre: "Macbook Air",
+            precio: 899,
+            foto: "",
+            id: 1
+        },
+        {
+            nombre: "Macbook Pro",
+            precio: 1999,
+            foto: "",
+            id: 2
+        },
+        {
+            nombre: "Mac Mini",
+            precio: 699,
+            foto: "",
+            id: 3
+        }
+    ]
 
-
-router.get('/', function(req, res) {
-    res.send("Hola desde el Router")
+router.get('/productos', function(req, res) {
+    res.send(productos);
   })
 
+  router.get('/productos/:id', function(req, res) {
+    let id = req.params.id;
+    res.send(productos[id-1])
+  })
+
+  router.post('/productos', function(req, res) {
+      let nuevoProducto = req.body;
+      nuevoProducto = {...nuevoProducto, id: (productos.length +1)}
+      console.log(productos.length)
+      productos.push(nuevoProducto)
+    res.send(productos)
+  })
+
+  router.put('/productos/:id', function(req, res) {
+    let id = req.params.id;
+    productos[id-1].nombre = req.body.nombre;
+    productos[id-1].precio = req.body.precio;
+    res.send(productos)
+  })
+
+  router.delete('/productos/:id', function(req, res) {
+    let id = req.params.id;
+    productos = productos.filter(item => item.id != id)
+    res.send(productos)
+  })
 module.exports = router;
